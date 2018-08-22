3572de27ddd05df658be63cc2e48e255572dbfaa
feixiang <feixiang@greatld.com>
2018-08-22 19:37:14 +0800

    测试

---
+++ src/main/java/com/mine/controller/DemoController.java
@@ -0,0 +1,11 @@
+package com.mine.controller;
+
+/**
+ * <简述>
+ *
+ * @Auther: feixiang
+ * @Date: 2018/8/22 19:35
+ * @Description:
+ */
+public class DemoController {
+}
---
+++ src/main/java/com/mine/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <简述>
+ *
+ * @Auther: feixiang
+ * @Date: 2018/8/22 19:35
+ * @Description:
+ */
+package com.mine;
